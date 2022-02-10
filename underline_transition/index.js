59778ec359048b018eb8756fd3d6e9f7fea8925b
Andrew Kliuiko <akliuiko93@gmail.com>
2022-02-10 08:35:19 +0200

    Initial position added

--- underline_transition/index.js
+++ underline_transition/index.js
@@ -1,9 +1,22 @@
-document.querySelectorAll("a").forEach(link => link.onmouseover = (e) => {
+const links = document.querySelectorAll("a");
+const slider = document.querySelector(".underline");
+
+const getInitialPosition = (element) => {
+	return {x, y, width, height} = element.getBoundingClientRect();
+} 
+
+const initPosition = getInitialPosition(links[0]);
+
+slider.style.width = `${initPosition.width}px`;
+slider.style.height = `${initPosition.height}px`;
+slider.style.transform = `translate(${initPosition.x}px, ${initPosition.y}px)`;
+
+links.forEach(link => link.onmouseover = (e) => {
 	const link = e.target.getBoundingClientRect();
-	const height = `${link.bottom - link.top}px`;
-	const width = `${link.right - link.left}px`;
+	const height = `${link.height}px`;
+	const width = `${link.width}px`;
 	const translate = `translate(${link.x}px, ${link.y}px)`;
-	const slider = document.querySelector(".underline");
+
 	slider.style.transform = translate;
 	slider.style.width = width;
 	slider.style.height = height;
